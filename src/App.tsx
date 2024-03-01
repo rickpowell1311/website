4dddd729416a72c1118a560c637abf60c595f502
Rick <rickpowell1311@gmail.com>
2024-03-01 18:19:11 +0000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+const App = () => {
+  return (
+    <>
+      <header className="w-screen">
+
+      </header>
+      <main className="w-screen">
+        <div className="container mx-auto">
+          <h1 className="text-3xl font-bold underline">
+            Hello world!
+          </h1>
+        </div>
+      </main>
+    </>
+  )
+}
+
+export default App
