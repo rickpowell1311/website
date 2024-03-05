a7f3975d2cbd406569903e3ef7a0bdd9f292e587
Rick <rick@propellenetwork.com>
2024-03-05 14:24:20 +0000

    Only highlight tiles on hover

--- src/components/launcher/launcher.tsx
+++ src/components/launcher/launcher.tsx
@@ -1,6 +1,6 @@
 const Tile = ({children = undefined as any}) => {
     return (
-        <div className="border-slate-400 border-solid border-2 rounded-3xl w-32 h-32 bg-slate-500 hover:scale-110 duration-300 hover:border-slate-200 cursor-pointer hover:shadow-lg hover:drop">
+        <div className="border-slate-400 rounded-3xl w-32 h-32 hover:scale-110 duration-300 hover:border-2 hover:border-slate-200 hover:bg-slate-500 cursor-pointer hover:shadow-lg hover:drop">
             <div className="flex justify-center items-center w-full h-full">
                 { children }
             </div>
